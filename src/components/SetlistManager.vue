7efc15895e2978a6a6133e98a2a64886dd976153
Sam Vevang <sam.vevang@gmail.com>
2018-05-14 11:30:38 -0500

    create a song

---
+++ src/components/SetlistManager.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Setlist Manager:</h1>
+    <p><SongCreate/></p>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import SongCreate from "./SongCreate.vue"
+
+export default {
+  data: ()=> {
+    return {
+      greeting: "Hello0"
+    }
+  },
+  components: {
+    SongCreate
+  }
+};
+</script>
+
+<style scoped>
+</style>
