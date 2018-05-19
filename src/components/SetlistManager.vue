5144498e18f65760683f2b48bb95a4155db3377a
Sam Vevang <sam.vevang@gmail.com>
2018-05-19 15:24:11 -0500

    print out a list of added songs

--- src/components/SetlistManager.vue
+++ src/components/SetlistManager.vue
@@ -2,21 +2,30 @@
   <div>
     <h1>Setlist Manager:</h1>
     <p><SongCreate/></p>
+    <p><SongList v-bind:songs="songs" /></p>
+
   </div>
 </template>
 
 <script>
 import Vue from "vue";
+import { mapState } from 'vuex'
+
 import SongCreate from "./SongCreate.vue"
+import SongList from "./SongList.vue"
 
 export default {
   data: ()=> {
     return {
-      greeting: "Hello0"
     }
   },
   components: {
-    SongCreate
+    SongCreate,
+    SongList,
+  },
+  computed: {
+    // set up the 'computed' accessors, e.g. `songs`
+    ...mapState(['songs'])
   }
 };
 </script>
---
+++ src/components/SongList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4 v-if="songs.length > 0">Song List</h4>
+    <pre v-for="song in songs">
+      {{song}}
+    </pre>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['songs'],
+};
+</script>
+
+<style scoped>
+</style>
