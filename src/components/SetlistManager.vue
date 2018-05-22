409fa8d1e1b8bd604ebb724bf79e2de69ed56158
Sam Vevang <sam.vevang@gmail.com>
2018-05-22 11:41:18 -0500

    include bootstrap styles

--- src/components/SetlistManager.vue
+++ src/components/SetlistManager.vue
@@ -30,5 +30,23 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.column {
+  margin-top: 30px;
+  width: 340px;
+  background: #e2e4e6;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+
+  height: 100%;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: top;
+}
+.column:first-child{
+  margin-left: 30px;
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,18 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                    use: [
+                        "style-loader", // creates style nodes from JS strings
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS
+                    ]
             }
         ],
     },
